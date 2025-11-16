4db95f188a3bc78991042454d9cf5b1e8f74a917
dianarhmaw <dianarhm12@gmail.com>
2025-11-16 22:49:30 +0700

    W12: Jawaban Soal 2

---
+++ Pertemuan-12/src/stream_diana/lib/stream.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorStream{
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.pink,
+    Colors.lightGreen,
+    Colors.cyan,
+    Colors.lime,
+    Colors.brown,
+  ];
+
+}
