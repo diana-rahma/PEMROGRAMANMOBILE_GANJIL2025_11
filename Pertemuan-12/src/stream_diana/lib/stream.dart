63cba5f3e7b046c97377d33d78627ca787f97337
dianarhmaw <dianarhm12@gmail.com>
2025-11-16 23:16:19 +0700

    prak 1

--- Pertemuan-12/src/stream_diana/lib/stream.dart
+++ Pertemuan-12/src/stream_diana/lib/stream.dart
@@ -14,4 +14,12 @@ class ColorStream{
     Colors.brown,
   ];
 
+  Stream<Color> getColor() async* {
+    yield* Stream.periodic(
+      const Duration(seconds: 1), (int t) {
+        int index = t % colors.length;
+        return colors[index];
+    });
+  }
+
 }
