7daf98faf11fc094a62c6d9da5424dfabce86b06
dianarhmaw <dianarhm12@gmail.com>
2025-11-16 23:52:13 +0700

    prak 3

--- Pertemuan-12/src/stream_diana/lib/main.dart
+++ Pertemuan-12/src/stream_diana/lib/main.dart
@@ -33,13 +33,22 @@ class _StreamHomePageState extends State<StreamHomePage> {
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
+  late StreamTransformer transformer;
 
   @override
   void initState() {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream
+     transformer = StreamTransformer<int, int>.fromHandlers(
+    handleData: (value, sink) {
+      sink.add(value * 10);
+    },
+    handleError: (error, trace, sink) {
+      sink.add(-1);
+    },
+    handleDone: (sink) => sink.close());
+    stream.transform(transformer)
         .listen((event) {
           setState(() {
             lastNumber = event;
@@ -69,9 +78,9 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   void addRandomNumber() {
     Random random = Random();
-    //int myNum = random.nextInt(10);
-    //numberStream.addNumberToSink(myNum);
-    numberStream.addError();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+    // numberStream.addError();
   }
 
   @override
