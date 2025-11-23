e8403bd37a8c0ba45d36aba4942487cb2d699a5e
dianarhmaw <dianarhm12@gmail.com>
2025-11-24 00:28:30 +0700

    pertemuan 13

---
+++ Pertemuan-13/src/store_data_diana/lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import './model/pizza.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Store Data - Diana',
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Pizza> myPizzas = [];
+  String pizzaString = '';
+
+  // Future readJsonFile() async {
+  //   String myString = await DefaultAssetBundle.of(
+  //     context,
+  //   ).loadString('assets/pizzalist.json');
+  //   setState(() {
+  //     return myPizzas;
+  //     // pizzaString = myString;
+  //   });
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile().then((value) {
+      setState(() {
+        myPizzas = value;
+      });
+    });
+  }
+
+  Future<List<Pizza>> readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/pizzalist.json');
+    List pizzaMapList = jsonDecode(myString);
+    List<Pizza> myPizzas = [];
+    for (var pizza in pizzaMapList) {
+      Pizza myPizza = Pizza.fromJson(pizza);
+      myPizzas.add(myPizza);
+    }
+    String json = convertToJson(myPizzas);
+    print(json);
+    return myPizzas;
+  }
+
+  String convertToJson(List<Pizza> pizzas) {
+    return jsonEncode(pizzas.map((pizza) => jsonEncode(pizza)).toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('JSON - Diana')),
+      body: Text(pizzaString),
+    );
+  }
+}
