78b1641b40aaa4ddc4902dce37521ff00af90e84
dianarhmaw <dianarhm12@gmail.com>
2025-12-01 18:55:53 +0700

    praktikum 14

---
+++ Pertemuan-14/src/store_data_diana/lib/model/httphelper.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'pizza.dart';
+
+class HttpHelper {
+  final String authority = '02z2g.mocklab.io';
+  final String path = 'pizzalist';
+  static final HttpHelper _httpHelper = HttpHelper._internal();
+  HttpHelper._internal();
+  factory HttpHelper() {
+    return _httpHelper;
+  }
+
+  Future<String> postPizza(Pizza pizza) async {
+    const postPath = '/pizza';
+    String post = json.encode(pizza.toJson());
+    Uri url = Uri.https(authority, postPath);
+    http.Response r = await http.post(url, body: post);
+    return r.body;
+  }
+
+  Future<List<Pizza>> getPizzaList() async {
+    final Uri url = Uri.https(authority, path);
+    final http.Response result = await http.get(url);
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      //provide a type argument to the map method to avoid type
+      //error
+      List<Pizza> pizzas = jsonResponse
+          .map<Pizza>((i) => Pizza.fromJson(i))
+          .toList();
+      return pizzas;
+    } else {
+      return [];
+    }
+  }
+}
---
+++ Pertemuan-14/src/store_data_diana/lib/model/pizza_detail.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'pizza.dart';
+import 'httphelper.dart';
+
+final TextEditingController txtId = TextEditingController();
+final TextEditingController txtName = TextEditingController();
+final TextEditingController txtDescription = TextEditingController();
+final TextEditingController txtPrice = TextEditingController();
+final TextEditingController txtImageUrl = TextEditingController();
+String operationResult = '';
+
+class PizzaDetailScreen extends StatefulWidget {
+  const PizzaDetailScreen({super.key});
+  @override
+  State<PizzaDetailScreen> createState() => _PizzaDetailScreenState();
+}
+
+class _PizzaDetailScreenState extends State<PizzaDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
+
+
